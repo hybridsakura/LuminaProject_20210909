e22b28844da1e2fc933a1779c3f71452e187f9ec
hybridsakura <hybridsakura@outlook.com>
2021-09-09 17:42:51 +0800

    20210909常规代码更新 目前已增加读取xlsx的方法，等待实际内容编写

---
+++ src/main/java/com/hybridsakura/project/app/luminabot/repo/ProfileData.java
@@ -0,0 +1,92 @@
+package com.hybridsakura.project.app.luminabot.repo;
+
+import java.io.Serializable;
+
+public class ProfileData implements Serializable {
+
+    private static final long serialVersionUID = 2422802823469417292L;
+
+    //  配置记录类
+
+    private String uuid;                                                            //  记录全局ID
+    private String operator;                                                        //  操作者
+    private String createDateTime;                                                  //  记录创建时间
+    private String updateDateTime;                                                  //  记录更新时间
+    private String reqMasterKeyword;                                                //  侦测匹配主关键字
+    private String reqSecondKeyword;                                                //  侦测匹配次关键字
+    private Boolean reqMasterMatched;                                               //
+    private Boolean reqSecondMatched;                                                //
+    private String respKeyword;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public void setCreateDateTime(String createDateTime) {
+        this.createDateTime = createDateTime;
+    }
+
+    public String getUpdateDateTime() {
+        return updateDateTime;
+    }
+
+    public void setUpdateDateTime(String updateDateTime) {
+        this.updateDateTime = updateDateTime;
+    }
+
+    public String getReqMasterKeyword() {
+        return reqMasterKeyword;
+    }
+
+    public void setReqMasterKeyword(String reqMasterKeyword) {
+        this.reqMasterKeyword = reqMasterKeyword;
+    }
+
+    public String getReqSecondKeyword() {
+        return reqSecondKeyword;
+    }
+
+    public void setReqSecondKeyword(String reqSecondKeyword) {
+        this.reqSecondKeyword = reqSecondKeyword;
+    }
+
+    public Boolean getReqMasterMatched() {
+        return reqMasterMatched;
+    }
+
+    public void setReqMasterMatched(Boolean reqMasterMatched) {
+        this.reqMasterMatched = reqMasterMatched;
+    }
+
+    public Boolean getReqSecondMatched() {
+        return reqSecondMatched;
+    }
+
+    public void setReqSecondMatched(Boolean reqSecondMatched) {
+        this.reqSecondMatched = reqSecondMatched;
+    }
+
+    public String getRespKeyword() {
+        return respKeyword;
+    }
+
+    public void setRespKeyword(String respKeyword) {
+        this.respKeyword = respKeyword;
+    }
+}
---
+++ src/main/java/com/hybridsakura/project/app/luminabot/repo/ProfileHandle.java
@@ -0,0 +1,26 @@
+package com.hybridsakura.project.app.luminabot.repo;
+
+public class ProfileHandle {
+
+    //  负责将配置文件读取，并回写到文件中
+
+
+    //  负责将程序内部的配置文件读取，写入到外部xlsx文件中
+    public void outputProfile2File(String targetFilepath) {
+
+        ProfileData profileData = new ProfileData();
+
+
+
+    }
+
+    //  负责读取外部的xlsx文件，并依次封装成配置对象
+    public void inputFileToProfile(String targetFilepath) {
+
+
+
+    }
+
+
+
+}
